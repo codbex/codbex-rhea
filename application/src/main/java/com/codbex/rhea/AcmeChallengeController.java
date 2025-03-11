10aec0d4c9f348c92c05b80b36df83b5cd075a31
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-11 21:38:12 +0200

    Add acme-challenge REST (#174)

---
+++ application/src/main/java/com/codbex/rhea/AcmeChallengeController.java
@@ -0,0 +1,15 @@
+package com.codbex.rhea;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+class AcmeChallengeController {
+
+    @GetMapping("/.well-known/acme-challenge/{token}")
+    ResponseEntity<String> handleAcmeChallenge(@PathVariable("token") String token) {
+        return ResponseEntity.ok("challenge-token-content"); // Return the challenge content for the token
+    }
+}
