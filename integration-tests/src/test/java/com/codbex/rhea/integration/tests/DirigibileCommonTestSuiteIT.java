3f3eefc824fac6811450ffc55bad1a5fe4c0162e
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-31 16:15:26 +0300

    remove not needed tests

--- integration-tests/src/test/java/com/codbex/rhea/integration/tests/DirigibileCommonTestSuiteIT.java
+++ integration-tests/src/test/java/com/codbex/rhea/integration/tests/DirigibileCommonTestSuiteIT.java
@@ -2,8 +2,6 @@ package com.codbex.rhea.integration.tests;
 
 import org.eclipse.dirigible.integration.tests.api.SecurityIT;
 import org.eclipse.dirigible.integration.tests.api.java.messaging.MessagingFacadeIT;
-import org.eclipse.dirigible.integration.tests.api.javascript.cms.CmsSuiteIT;
-import org.eclipse.dirigible.integration.tests.api.rest.ODataAPIIT;
 import org.eclipse.dirigible.integration.tests.ui.tests.GitPerspectiveIT;
 import org.eclipse.dirigible.integration.tests.ui.tests.HomepageRedirectIT;
 import org.eclipse.dirigible.integration.tests.ui.tests.MailIT;
@@ -13,12 +11,10 @@ import org.junit.platform.suite.api.Suite;
 
 @Suite
 @SelectClasses({//
-        CmsSuiteIT.class, //
         GitPerspectiveIT.class, //
         HomepageRedirectIT.class, //
         MailIT.class, //
         MessagingFacadeIT.class, //
-        ODataAPIIT.class, //
         SecurityIT.class, //
         TerminalIT.class//
 })
